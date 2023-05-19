a566a51adb36babd2379124f509e871ddd6a31e0
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-05-19 20:15:20 +0900

    Remove Unnecessary things.

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -4,40 +4,11 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3 border-gray-500">
       <div className="text-gray-500">
-        Powered by{" "}
-        <a
-          href="https://replicate.com/"
-          target="_blank"
-          className="font-bold hover:underline transition hover:text-gray-300 underline-offset-2"
-        >
-          Replicate,{" "}
-        </a>
-        <a
-          href="https://upload.io"
-          target="_blank"
-          className="font-bold hover:underline hover:text-gray-300 transition underline-offset-2"
-        >
-          Upload,{" "}
-        </a>
-        <a
-          href="https://loops.so"
-          target="_blank"
-          className="font-bold hover:underline hover:text-gray-300 transition underline-offset-2"
-        >
-          Loops,{" "}
-        </a>
-        and{" "}
-        <a
-          href="https://vercel.com/"
-          target="_blank"
-          className="font-bold hover:underline transition hover:text-gray-300 underline-offset-2"
-        >
-          Vercel.
-        </a>
+        Contact us : komon@gmail.com
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://twitter.com/komon"
           className="group"
           aria-label="TaxPal on Twitter"
         >
